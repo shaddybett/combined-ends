14fb11fbffe1bc3d6d36571957faf61f2c90b4df
Shaddy <shadybett540@gmail.com>
2024-01-31 21:26:02 +0300

    installed React-router -dom

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
     <div>
-        <h1>Welcome Home</h1>
-
+      <h1>Welcome Home</h1>
+      <a href="/signup">Register</a>
     </div>
-  )
+  );
 }
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Signup() {
   return (
     <div>
-        <h1>This is signup hi there!</h1>
+      <h1>This is signup hi there!</h1>
+      <a href="/">home</a>
+      <a href="/about">About</a>
     </div>
-  )
+  );
 }
