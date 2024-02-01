ef965ae9820be62ec5da3a72d768d9538e9a9483
Shaddy <shadybett540@gmail.com>
2024-02-01 09:02:17 +0300

    made users table

--- server/Models.py
+++ server/Models.py
@@ -1,3 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__='users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(100),nullable=False)
+    email = db.Column(db.String(100),nullable=False)
+    password = db.Column(db.Integer,nullable=False)
+    
