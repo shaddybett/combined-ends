a315de09ef8b3b2517d3a10bc163a75aa3eebdc7
Shaddy <shadybett540@gmail.com>
2024-02-01 10:24:24 +0300

    made User class

--- server/App.py
+++ server/App.py
@@ -7,3 +7,15 @@ api = Api(app)
 
 api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///combined.db'
 api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.init_app(app)
+
+class User(Resource):
+    def get(self):
+        username = User.query.filter_by(id=id).first()
+        if username:
+            return username
+        else:
+            return ('User not found')
+
+
+api.addResource(User,'/user')
