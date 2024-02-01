f92e0f896286c01abcf27d790ab491b45d11b2f5
Shaddy <shadybett540@gmail.com>
2024-02-01 20:51:20 +0300

    Included cors for easy accces from other ports

--- server/app.py
+++ server/app.py
@@ -3,10 +3,11 @@ from flask_restful import Api,Resource
 from Models import db,User
 from flask_migrate import Migrate
 from flask_bcrypt import bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
-
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///combined.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
@@ -14,6 +15,8 @@ migrate = Migrate(app,db)
 
 def hash_password(password):
     salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
 
 
 class UserResource(Resource):
@@ -21,15 +24,15 @@ class UserResource(Resource):
         data = request.get_json()
         existing_User = User.query.filter_by(username = data['username']).first()
         if existing_User:
-            if existing_User.password == data['password']:
+            if bcrypt.checkpw(data['password'].encode('utf-8'),existing_User.password):
                 return {'message':'Succesfully logged in'},200
             else:
-                return {'message': 'Incorrect password'},401   
+                return {'message': 'Invalid credentials'},401   
         else:
-            new_user = User(username = data['username'], email=data['email'],password=data['password'])
+            new_user = User(username = data['username'], email=data['email'],password=hash_password(data['password']))
             db.session.add(new_user)
             db.session.commit()
-            return {'message':'Registration successfull'},200            
+            return {'message':'Registration successfull'},201            
 
 
 api.add_resource(UserResource,'/user')
