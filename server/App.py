dc1bb50db7b9f1a56ed5f0fc890069443fe0f9e9
Shaddy <shadybett540@gmail.com>
2024-02-01 09:22:08 +0300

    configurea api

--- server/App.py
+++ server/App.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import Api,Resource
-from Models import db
+from Models import db,User
 
 app = Flask(__name__)
 api = Api(app)
 
+api.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///combined.db'
+api.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
