6de4ba57ad19bdb6d56469926d5e3da886b3eb58
Shaddy <shadybett540@gmail.com>
2024-02-04 22:45:50 +0300

    fixed a typo

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import About from "./About";
+import About from "./About";
 
 export default function Home() {
   const [username, setUsername] = useState("");
