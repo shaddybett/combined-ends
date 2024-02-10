17f337718697d9478797e9f39ca247b3d39d9f68
Shaddy <shadybett540@gmail.com>
2024-02-11 02:37:46 +0300

    made basic js data

--- client2/src/components/Home.js
+++ client2/src/components/Home.js
@@ -33,5 +33,14 @@
 
 
 
-import React,{useState,useEffect} from "react";
 
+
+import React,{useState,useEffect} from 'react'
+
+function Home() {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
