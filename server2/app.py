3304cb58544241e73f69cdf570c0ce5af9ff5986
Shaddy <shadybett540@gmail.com>
2024-02-10 08:30:45 +0300

    done

--- server2/app.py
+++ server2/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/home')
 def home():
-    
+    members  = ['member1','member2','member3','member4']
+    return {'members':members}
 
 if __name__=='__main__':
     app.run(debug=True)
