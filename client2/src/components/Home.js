fdbcd6cbc3a82b4ca454be1f76a89f9ad22b6094
Shaddy <shadybett540@gmail.com>
2024-02-10 20:21:03 +0300

    done

--- client2/src/components/Home.js
+++ client2/src/components/Home.js
@@ -1,32 +1,32 @@
-import React,{useState,useEffect} from 'react'
+// import React,{useState,useEffect} from 'react'
 
-export default function Home() {
-    const [data,setData] = useState([])
+// export default function Home() {
+//     const [data,setData] = useState([])
 
-    useEffect(()=>{
-        fetch('http://localhost:5000/home')
-        .then((response)=> {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            else {
-                return response.json();
-            }
-        })
-        .then((data)=>setData(data.members))
-        .catch((error)=>{console.error('Error fetching data:', error)});
+//     useEffect(()=>{
+//         fetch('http://localhost:5000/home')
+//         .then((response)=> {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             else {
+//                 return response.json();
+//             }
+//         })
+//         .then((data)=>setData(data.members))
+//         .catch((error)=>{console.error('Error fetching data:', error)});
     
         
-    },[])
-  return (
-    <div>
-        {data ? (
-            <ul>
-                {data.map((member,index)=> (
-                    <li key={index}>{member}</li>
-                ))}
-            </ul>
-        ):(<p>Loading...</p>)}
-    </div>
-  )
-}
+//     },[])
+//   return (
+//     <div>
+//         {data ? (
+//             <ul>
+//                 {data.map((member,index)=> (
+//                     <li key={index}>{member}</li>
+//                 ))}
+//             </ul>
+//         ):(<p>Loading...</p>)}
+//     </div>
+//   )
+// }
