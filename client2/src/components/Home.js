261b1e9d8b4817be8dbe2483b30a4ae43cc7d6ef
Shaddy <shadybett540@gmail.com>
2024-02-10 08:09:20 +0300

    made state variables for data storage

--- client2/src/components/Home.js
+++ client2/src/components/Home.js
@@ -1,9 +1,11 @@
 import React,{useState,useEffect} from 'react'
 
-
-
 export default function Home() {
+    const [data,setData] = useState([])
 
+    useEffect(()=>{
+        fetch('http')
+    },[])
   return (
     <div>Home</div>
   )
