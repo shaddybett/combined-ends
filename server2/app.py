1f7367fb2950facbf08eedf42f76b7f8179feff6
Shaddy <shadybett540@gmail.com>
2024-02-10 08:26:05 +0300

    cleared errors

--- server2/app.py
+++ server2/app.py
@@ -1,11 +1,10 @@
-from flask import Flask,jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
-    members = {'member1','member2','member3'}
-    return jsonify ({members: list(members)})
+    
 
 if __name__=='__main__':
     app.run(debug=True)
