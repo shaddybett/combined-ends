5688cf4ef5004b0f5a77d1e38ee596b3630aff6c
Shaddy <shadybett540@gmail.com>
2024-02-10 13:18:13 +0300

    done

--- client2/src/components/Home.js
+++ client2/src/components/Home.js
@@ -4,9 +4,29 @@ export default function Home() {
     const [data,setData] = useState([])
 
     useEffect(()=>{
-        fetch('http://localhost:5000/members')
+        fetch('http://localhost:5000/home')
+        .then((response)=> {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            else {
+                return response.json();
+            }
+        })
+        .then((data)=>setData(data.members))
+        .catch((error)=>{console.error('Error fetching data:', error)});
+    
+        
     },[])
   return (
-    <div>Home</div>
+    <div>
+        {data ? (
+            <ul>
+                {data.map((member,index)=> (
+                    <li key={index}>{member}</li>
+                ))}
+            </ul>
+        ):(<p>Loading...</p>)}
+    </div>
   )
 }
