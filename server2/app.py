da17424ecb68aec44add2801e93b377276e002f2
Shaddy <shadybett540@gmail.com>
2024-02-11 14:54:21 +0300

    did a recap on app.py

--- server2/app.py
+++ server2/app.py
@@ -1,34 +1,43 @@
-from flask import Flask,request,jsonify
-from flask_cors import CORS
-from models import Member,db
+# from flask import Flask,request,jsonify
+# from flask_cors import CORS
+# from models import Member,db
+# from flask_restful import Api
+
+# app = Flask(__name__)
+# api = Api(app)
+# CORS(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///member.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db.init_app(app)
+
+# @app.route('/members', methods = ['POST'])
+# def signup ():
+#     data = request
+#     email = data.get(Member.email)
+#     existingUser = Member.query.filter_by(email=email).first()
+#     if existingUser:
+#         return jsonify({'error':'email already exists'})
+#     else:
+#         newUser = {
+#             'username':'username',
+#             'email':'email',
+#             'password':'pasword'
+#         }
+#     db.session.add(newUser)
+#     db.session.commit()    
+
+
+# if __name__=='__main__':
+#     app.run(debug=True)
+
+
+
+
+
+
+from flask import Flask
 from flask_restful import Api
+from models import Member,db
 
 app = Flask(__name__)
 api = Api(app)
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///member.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
-
-@app.route('/members', methods = ['POST'])
-def signup ():
-    data = request
-    email = data.get(Member.email)
-    existingUser = Member.query.filter_by(email=email).first()
-    if existingUser:
-        return jsonify({'error':'email already exists'})
-    else:
-        newUser = {
-            'username':'username',
-            'email':'email',
-            'password':'pasword'
-        }
-    db.session.add(newUser)
-    db.session.commit()    
-
-
-if __name__=='__main__':
-    app.run(debug=True)
-
-
-
