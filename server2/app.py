6d0c61837b3edc40747ea227b01aa0d9ebbd6319
Shaddy <shadybett540@gmail.com>
2024-02-11 15:32:10 +0300

    fixed all issues succesfully

--- server2/app.py
+++ server2/app.py
@@ -1,66 +1,33 @@
-# from flask import Flask,request,jsonify
-# from flask_cors import CORS
-# from models import Member,db
-# from flask_restful import Api
-
-# app = Flask(__name__)
-# api = Api(app)
-# CORS(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///member.db'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db.init_app(app)
-
-# @app.route('/members', methods = ['POST'])
-# def signup ():
-#     data = request
-#     email = data.get(Member.email)
-#     existingUser = Member.query.filter_by(email=email).first()
-#     if existingUser:
-#         return jsonify({'error':'email already exists'})
-#     else:
-#         newUser = {
-#             'username':'username',
-#             'email':'email',
-#             'password':'pasword'
-#         }
-#     db.session.add(newUser)
-#     db.session.commit()    
-
-
-# if __name__=='__main__':
-#     app.run(debug=True)
-
-
-
-
-
 
 from flask import Flask,request,jsonify
 from flask_restful import Api
 from models import Member,db
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite://member.db'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///member.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db.init_app(app)
 
-@app.route('/home',methods=['POST'])
-def signup(self):
-    data = request
-    email = data.get(Member.email)
-    existingUser = Member.query.filter_by(email=email).first()
-    if existingUser:
-        return jsonify ({'error':'User already exists'}),404
+@app.route('/signup',methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data.get('email')
+    existing_member = Member.query.filter_by(email=email).first()
+    if existing_member:
+        return jsonify ({'error':'Email already exists'}),400
     else:
-        newUser = {
-            'username':'username',
-            'email':'email',
-            'password':'password'
-        } 
-        db.session.add(newUser)
-        db.sessio.commit()
+        new_member = Member(
+            username = data.get('username'),
+            email=email,
+            password=data.get('password')
+        )
+        db.session.add(new_member)
+        db.session.commit()
+        return jsonify({'message':'Successfully signed in'}),200
 
 if __name__=='__main__':
     app.run(debug=True)        
