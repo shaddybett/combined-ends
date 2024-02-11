c966efd508405528e589719285ac610a940c4ce6
Shaddy <shadybett540@gmail.com>
2024-02-11 03:34:09 +0300

    done

--- server2/app.py
+++ server2/app.py
@@ -1,30 +1,14 @@
-# from flask import Flask
-# from  flask_cors import CORS
-
-# app = Flask(__name__)
-# CORS(app)
-# @app.route('/home')
-# def home():
-#     members  = ['member1','member2','member3','member4']
-#     return {'members':members}
-
-# if __name__=='__main__':
-#     app.run(debug=True)
-
-
-
-from flask import Flask
+from flask import Flask,request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/home')
-def members():
-    members=['alex','bett','norm']
-    return {'members': members}
-
+@app.route('/signup',method='POST')
+def signup():
+    data = request
+    
 
 if __name__=='__main__':
     app.run(debug=True)
