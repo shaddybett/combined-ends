54a20892a07f1dcf1ce1ab1a96bd549f80a37fec
Shaddy <shadybett540@gmail.com>
2024-02-12 08:18:07 +0300

    added error responses

--- client2/src/components/Signup.js
+++ client2/src/components/Signup.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+
 
 export default function Signup() {
+    const[user,setUser] = useState('')
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/signup')
+        .then ((response)=>{
+            if(!response.ok) {
+                throw new Error ('Signup unsuccesfull'),400
+            }
+            else {
+                return jsonify({'message':'successfully signed in'}),200
+            }
+           
+        })
+        .then ((user)=>setUser(user)) 
+        .catch((error)=>console.error('Error signing in')),404
+    },[])
   return (
-    <div></div>
+    <div>
+        
+    </div>
   )
 }
