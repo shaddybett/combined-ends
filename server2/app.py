18e136d3d3dfa984c0be2d84f29aa8ec1c116567
Shaddy <shadybett540@gmail.com>
2024-02-12 20:14:24 +0300

    done

--- server2/app.py
+++ server2/app.py
@@ -1,36 +1,77 @@
 
+# from flask import Flask,request,jsonify
+# from flask_restful import Api
+# from models import Member,db
+# from flask_cors import CORS
+# from flask_migrate import Migrate
+
+
+# app = Flask(__name__)
+# api = Api(app)
+# CORS(app)
+# Migrate(app,db)
+
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///member.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+# db.init_app(app)
+
+# @app.route('/signup',methods=['POST'])
+# def signup():
+#     data = request.get_json()
+#     email = data.get('email')
+#     existing_member = Member.query.filter_by(email=email).first()
+#     if existing_member:
+#         return jsonify ({'error':'Email already exists'}),400
+#     else:
+#         new_member = Member(
+#             username = data.get('username'),
+#             email=email,
+#             password=data.get('password')
+#         )
+#         db.session.add(new_member)
+#         db.session.commit()
+#         return jsonify({'message':'Successfully signed in'}),200
+
+# if __name__=='__main__':
+#     app.run(debug=True)        
+
+
+
+
+
 from flask import Flask,request,jsonify
 from flask_restful import Api
-from models import Member,db
+from models import db,Member
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///combine.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Migrate(app,db)
-
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///member.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 db.init_app(app)
 
 @app.route('/signup',methods=['POST'])
 def signup():
     data = request.get_json()
     email = data.get('email')
-    existing_member = Member.query.filter_by(email=email).first()
-    if existing_member:
-        return jsonify ({'error':'Email already exists'}),400
+    existingUsers = Member.query.filter_by(email=email).first()
+    if existingUsers:
+        return jsonify({'error':'Email already exists'}),400
     else:
-        new_member = Member(
+        newUser = Member(
             username = data.get('username'),
             email=email,
-            password=data.get('password')
+            password = data.get('password')
         )
-        db.session.add(new_member)
+        
+        db.session.add(newUser)
         db.session.commit()
-        return jsonify({'message':'Successfully signed in'}),200
+        return jsonify({'message':'Succesfully signed in'}),200
 
 if __name__=='__main__':
     app.run(debug=True)        
+
+
