b673329f79e0115c915c2d954e16903b3b0440d4
Shaddy <shadybett540@gmail.com>
2024-02-12 08:50:54 +0300

    made a function to handle submission

--- client2/src/components/Signup.js
+++ client2/src/components/Signup.js
@@ -7,30 +7,52 @@ export default function Signup() {
     const [pass,setPass] = useState('')
 
     const handleSignup = ()=> {
-
-    }
-    useEffect(()=>{
-        fetch('http://localhost:5000/signup')
-        .then ((response)=>{
-            if(!response.ok) {
-                throw new Error ('Signup unsuccesfull'),400
-            }
-            else {
-                return jsonify({'message':'successfully signed in'}),200
-            }
-           
+        fetch('http://localhost:5000/signup', {
+            method: 'POST',
+            headers:{
+                'Content_Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username:user,
+                email:email,
+                password:pass
+            }),
         })
-        .then ((data)=>setData(data)) 
-        .catch((error)=>console.error('Error signing in:',error)),404
-    },[])
-  return (
-    <div>
-        <input type='text' placeholder='Enter your Username' value={user} ></input>
-        <input type='email' placeholder='Enter your email' value={email} ></input>
-        <input type='password' placeholder='Enter your password' value={pass} ></input>
-    </div>
-  )
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Signup Unsuccessful');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error=> {
+            console.error('Error signing up:', error);
+        });
+
+    };
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
