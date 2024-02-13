a19d57f2fee638591704bc32236e08750c2a470e
Shaddy <shadybett540@gmail.com>
2024-02-13 23:19:49 +0300

    done

--- client2/src/App.js
+++ client2/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Home from './components/Home'
-import Signup from './components/Signup'
-import {Routes, Route} from "react-router-dom"
+import React from "react";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/signup' element={<Signup/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
-  )
+  );
 }
