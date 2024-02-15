e606572ef533c37a10d8011514fef6985a1d666c
Shaddy <shadybett540@gmail.com>
2024-02-15 23:12:51 +0300

    made a link in the Home page for redirection to the Login page

--- client2/src/components/Home.js
+++ client2/src/components/Home.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
-        <h1>Welcome to my app</h1>
-        <Link to='/signup'>Sign Up</Link>
+      <h1>Welcome to my app</h1>
+      <Link to="/signup">Sign Up</Link>
+      <Link to="/login">Login</Link>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
