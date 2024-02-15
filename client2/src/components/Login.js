76cec55d398d3b8868148b416d924b1920b18529
Shaddy <shadybett540@gmail.com>
2024-02-15 23:42:19 +0300

    added a form submit

--- client2/src/components/Login.js
+++ client2/src/components/Login.js
@@ -11,12 +11,14 @@ function Login() {
         methods = 'GET',
         headers = Application/json)
     },[])
-
+    
   return (
     <div>
         <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
         <input type='text' placeholder='Enter your username' value={user} onChange={setUser} />
         <input type='password' placeholder='Enter your password' value={password} onChange={setPassword} />
+        </form>
     </div>
   )
 }
